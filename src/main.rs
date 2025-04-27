e13ceee34db699f2b967fe8f9dd4a0cd67b5ca2d
waldrumpus <waldrumpus@gmail.com>
2025-04-27 12:15:57 +0200

    add code documentation

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,97 @@
-use std::f32::consts::{FRAC_PI_2, PI};
+use std::{cmp, f32::consts::{FRAC_PI_2, PI}};
 
 use macroquad::prelude::*;
 
+/// Size of a grid cell (circle)
 const W: f32 = 80.0;
 
 #[macroquad::main("Lissajous Curve Table")]
 async fn main() {
-    request_new_screen_size(700.0, 700.0);
+    // Scene setup
+    let mut scene = Scene::new();
+    let mut sw = screen_width();
+    let mut sh = screen_height();
 
-    let cols = (screen_width() / W).floor() as i32 - 1;
-    let rows = cols;
-    let mut col_circles = vec![];
-    let mut row_circles = vec![];
-    let mut curves = vec![];
-
-    // Create the column circles
-    for i in 0..cols {
-        let cx = (i as f32 + 1.5) * W;
-        let cy = W / 2.0;
-        let d = W / 2.0 - 10.0;
-        let color = Color::from_rgba((i as f32 * 256.0/cols as f32) as u8, 255, 0, 255);
-        col_circles.push(Circle::new(cx, cy, d, color));
-    }
-
-    // Create the row circles
-    for j in 0..rows {
-        let cy = (j as f32 + 1.5) * W;
-        let cx = W / 2.0;
-        let d = W / 2.0 - 10.0;
-        let color = Color::from_rgba(255, 0, (j as f32 * 256.0/cols as f32) as u8, 255);
-        row_circles.push(Circle::new(cx, cy, d, color));
-    }
-
-    // Create cols * rows amount of curves
-    for _ in 0..rows {
-        let mut row = vec![];
-        for _ in 0..cols {
-            row.push(Curve::new());
-        }
-        curves.push(row);
-    }
-
-    let mut scene = Scene { rows: row_circles, cols: col_circles, curves, angle: 0.0 };
-
+    // Drawing and updating loop
     loop {
         clear_background(BLACK);
         scene.show();
         next_frame().await;
         scene.update();
+
+        // If the screen size has changed,
+        // reset the scene to fit the new size
+        if screen_width() != sw || screen_height() != sh {
+            sw = screen_width();
+            sh = screen_height();
+            scene = Scene::new();
+        }
     }
 }
 
+/// Data structure to hold all objects in the scene
 struct Scene {
+    /// Row circles
     rows: Vec<Circle>,
+    /// Column circles
     cols: Vec<Circle>,
+    /// The rows and columns of curves being drawn
     curves: Vec<Vec<Curve>>,
+    /// The angle that marks the circles' progress
     angle: f32,
 }
 
 impl Scene {
+    /// Creates a new scene
+    fn new() -> Scene {
+        // Compute the number of rows and columns on screen
+        let cols = (screen_width() / W).floor() as i32 - 1;
+        let rows = (screen_height() / W).floor() as i32 - 1;
+        let cols = cmp::min(cols, rows);
+        let rows = cols;
+        let mut col_circles = vec![];
+        let mut row_circles = vec![];
+        let mut curves = vec![];
+
+        // Create the column circles
+        for i in 0..cols {
+            let cx = (i as f32 + 1.5) * W;
+            let cy = W / 2.0;
+            let d = W / 2.0 - 10.0;
+            let color = Color::from_rgba((i as f32 * 256.0/cols as f32) as u8, 255, 0, 255);
+            col_circles.push(Circle::new(cx, cy, d, color));
+        }
+
+        // Create the row circles
+        for j in 0..rows {
+            let cy = (j as f32 + 1.5) * W;
+            let cx = W / 2.0;
+            let d = W / 2.0 - 10.0;
+            let color = Color::from_rgba(255, 0, (j as f32 * 256.0/cols as f32) as u8, 255);
+            row_circles.push(Circle::new(cx, cy, d, color));
+        }
+
+        // Create cols * rows amount of curves
+        for _ in 0..rows {
+            let mut row = vec![];
+            for _ in 0..cols {
+                row.push(Curve::new());
+            }
+            curves.push(row);
+        }
+
+        Scene {
+            rows: row_circles,
+            cols: col_circles,
+            curves,
+            angle: 0.0,
+        }
+    }
+
+    /// Draws the scene on the screen
     fn show(&mut self) {
+        // Draw the horizontal row of circles
+        // Also update the circle progress
         for i in 0..self.cols.len() {
             let circle = &self.cols[i];
             draw_circle_lines(circle.x, circle.y, circle.d, 1.0, circle.color);
@@ -74,6 +106,8 @@ impl Scene {
             }
         }
 
+        // Draw the vertical column of circles
+        // Also update the circle progress
         for j in 0..self.rows.len() {
             let circle = &self.rows[j];
             draw_circle_lines(circle.x, circle.y, circle.d, 1.0, circle.color);
@@ -88,6 +122,7 @@ impl Scene {
             }
         }
 
+        // Update the curves with the current circle progress
         for j in 0..self.rows.len() {
             for i in 0..self.cols.len() {
                 self.curves[j][i].add();
@@ -96,6 +131,8 @@ impl Scene {
         }
     }
 
+    /// Update the scene by changing the angle
+    /// Angle is reset occasionally
     fn update(&mut self) {
         self.angle -= PI / 400.0;
         
@@ -110,26 +147,37 @@ impl Scene {
     }
 }
 
+/// A colored circle
 struct Circle {
+    /// X coordinate of the center of the circle
     x: f32,
+    /// Y coordinate of the center of the circle
     y: f32,
+    /// Diameter of the circle
     d: f32,
+    /// Color of the circle
     color: Color,
 }
 
 impl Circle {
+    /// Creates a new circle with a position, diameter, and color
     fn new(x: f32, y: f32, d: f32, color: Color) -> Circle {
         Circle { x, y, d, color }
     }
 }
 
+/// A colored point
 struct ColoredPoint {
+    /// X coordinate of the point
     x: f32,
+    /// Y coordinate of the point
     y: f32, 
+    /// Color of the point
     color: Color,
 }
 
 impl ColoredPoint {
+    /// Creates a new colored point with x, y, and color
     fn new(x: f32, y: f32, color: Color) -> ColoredPoint {
         ColoredPoint { x, y, color }
     }
@@ -137,9 +185,13 @@ impl ColoredPoint {
 
 /// A Lissajous curve represented as a sequence of points
 struct Curve {
+    /// The points that form the curve
     path: Vec<ColoredPoint>,
+    /// The point currently being constructed by the circles' progress
     current: Vec2,
+    /// The color of the circle that contributes the curve's x coordinate
     color_x: Color,
+    /// The color of the circle that contributes the curve's y coordinate
     color_y: Color,
 }
 
@@ -149,6 +201,7 @@ impl Curve {
         Curve { path: vec![], current: Vec2::ZERO, color_x: WHITE, color_y: WHITE }
     }
 
+    /// Reset the curve by removing all of its points
     fn reset(&mut self) {
         self.path.clear();
     }
@@ -188,6 +241,7 @@ impl Curve {
     }
 }
 
+/// Performs linear interpolation between to colors
 fn lerp(c1: &Color, c2: &Color) -> Color {
     let r = ((c1.r + c2.r) / 2.0) * 255.0;
     let g = ((c1.g + c2.g) / 2.0) * 255.0;
